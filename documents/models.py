515c4d9a22720781200df56de033c999bd6b1c34
Stewart Wallace <stewartw@gaelquality.com>
2017-11-19 13:56:29 +0000

    Adding in basic models for document & revision

--- documents/models.py
+++ documents/models.py
@@ -4,3 +4,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Document(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+class Revision(models.Model):
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
+    owner = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    attachment = models.CharField(max_length=400)
+    activated_date = models.DateTimeField('Date Activated')
+    removed_date = models.DateTimeField('Date Superseded / Archived')
---
+++ documents/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'documents'
+urlpatterns = [url(r'^$', views.index, name='index')]
